ffc40d3fb0e38e8ce852ce3b34c47049db4438ba
carlos-crew <66502581+carlos-crew@users.noreply.github.com>
2022-11-27 19:36:29 -0600

    Prueba

---
+++ sw.js
@@ -0,0 +1,75 @@
+importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+    );
+
+workbox.precaching.precacheAndRoute ([
+    'index.html',
+    'about.html',
+    'menu.html',
+    'cart.html',
+    'offline.html',
+    'icon/offline.jpg',
+    'css/style.css',
+    'checkout.html',
+    'contact.html',
+    'login.html',
+    'orders.html',
+    'profile.html',
+    'register.html',
+    'search.html',
+    'update_address.html',
+    'update_profile.html',
+    'app.js',
+    'sw.js',
+    'manifest.webmanifest',
+    'img',
+    'images',
+    'js/script.js'
+
+]);
+
+
+workbox.routing.registerRoute(
+    ({request})=> request.destination === 'image',
+    new workbox.strategies.NetworkOnly()
+);
+workbox.routing.registerRoute(
+    ({request}) => request.destination === 'document',
+    new workbox.strategies.NetworkFirst()
+);
+
+//cuando hay error
+workbox.routing.setCatchHandler(async context =>{
+    //console.log("entro");
+    console.log(context);
+    console.log(context.request);
+    if (context.request.destination === 'image'){
+        return workbox.precaching.matchPrecache('icon/offline.jpg');} 
+    else if(context.request.destination === 'document'){
+        return workbox.precaching.matchPrecache('offline.html');
+    }
+
+    return Response.error();
+});
+
+
+self.addEventListener('install', (e)=>{
+    console.log("Instalado");
+    e.waitUntil(async()=>{
+      const cache = await caches.open(cacheName);
+      await cache.addAll(contenidoCache);
+    })
+});
+
+//  CODIGO DE INSTALACION
+
+self.addEventListener('fetch',(e)=>{
+   e.respondWith((async()=>{
+        const r = await caches.match(e.request);
+        if (r) return r;
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        cache.put(e.request, response.clone());
+        return response;
+    })())
+});
